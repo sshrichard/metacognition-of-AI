867204a1adfbb29c82982d8671dba164011cbc3f
sshrichard <81316036+sshrichard@users.noreply.github.com>
2026-03-02 00:44:09 +0900

    Update model name to include version date

--- c_calibration_GPT.m
+++ c_calibration_GPT.m
@@ -37,7 +37,7 @@ base_PROMPT = fileread(prompt_name + ".txt");
 
 setenv('OPENAI_API_KEY','KEY'); % replace KEY by your API key 
 
-model_name = "gpt-5";
+model_name = "gpt-5-2025-08-07";
 temp = 1.0;
 responses = cell(numSamples, 1);
 
